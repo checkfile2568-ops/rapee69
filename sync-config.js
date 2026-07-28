8eb0f0da9a95b064dac9fff442f93eba185bfdfc
checkfile2568-ops <checkfile2568-ops@users.noreply.github.com>
2026-07-28 14:09:43 +0700

    Configure remote sync endpoint

--- sync-config.js
+++ sync-config.js
@@ -1,3 +1,3 @@
 // ใส่ URL ของ Google Apps Script Web App หลัง deploy หากต้องการซิงก์ข้ามเครื่อง
 // ตัวอย่าง: window.DRAW_REMOTE_SYNC_URL = "https://script.google.com/macros/s/DEPLOYMENT_ID/exec";
-window.DRAW_REMOTE_SYNC_URL = "";
+window.DRAW_REMOTE_SYNC_URL = "https://script.google.com/macros/s/AKfycbw0K_tqGZVoRINVOclJCs28YEtBTGoWyw_NA1FZE5rEEIgE0evgMNH29bmjS49m8gDGVA/exec";
